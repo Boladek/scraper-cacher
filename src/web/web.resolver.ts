97e213416402ec9edef6f9496ccf8bba913a6cdf
Boladek <mubarak.adekunle@decagon.dev>
2021-07-14 01:44:43 +0100

    added get requuest

--- src/web/web.resolver.ts
+++ src/web/web.resolver.ts
@@ -1,4 +1,5 @@
 import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
+import { GetSingleWebDetailsArgs } from './dto/get-single-web-details.dto';
 import { GetWebDetailsArgs } from './dto/get-web-details.dto';
 import { UrlToBeParsedInput } from './dto/parse-web.dto';
 import { WebDetails } from './model/web-details.interface';
@@ -18,5 +19,13 @@ export class WebResolver {
   @Query(() => [WebDetails], { name: 'parsedurls', nullable: 'items' })
   getWebDetails(@Args() getWebDetailsArgs: GetWebDetailsArgs): WebDetails[] {
     return this.webService.getWebDetails(getWebDetailsArgs);
+  }
+
+  @Query(() => WebDetails, { name: 'parsedurl', nullable: true })
+  getSingleWebDetails(
+    @Args() getSingleWebDetailsArgs: GetSingleWebDetailsArgs,
+  ): WebDetails {
+    return;
+    return this.webService.getSingleWebDetails(getSingleWebDetailsArgs);
   }
 }
